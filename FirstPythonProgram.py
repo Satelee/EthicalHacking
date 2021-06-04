35f1568c537a29462271876af1f0767e06a103df
Satele <soumita.kale@outlook.com>
2021-06-05 03:23:04 +0800

    Completed lecture 147 creating the first python program.

---
+++ FirstPythonProgram.py
@@ -0,0 +1,8 @@
+## Printing 'Hello There' ##
+print('Hello There!')
+
+
+## Progam to read user's name and display greeting ##
+# Note: input is a way to capture information
+name = input('What is your name? ') # typed information will get assigned to name variable 
+print('Nice to meet you ' + name + '!')
