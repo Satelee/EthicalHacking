1c7bd976d31f858f2e4e6be839fd217c9d485105
Satele <soumita.kale@outlook.com>
2021-06-12 15:03:51 +0800

    Started lecture 154

---
+++ 20. Learn Python Intermediate/Numbers.py
@@ -0,0 +1,8 @@
+# Fundamental Data Types #
+# data types are values in python.
+# a program is instructions that tell a computer what to do
+    # all about storing information or a data type and modifying it
+
+# int (interger) - represents all numbers
+
+# float 
