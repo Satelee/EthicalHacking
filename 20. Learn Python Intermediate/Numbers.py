ab9bc6eadadc08102f73c721c8dad8b161b7bb8b
Satele <soumita.kale@outlook.com>
2021-06-13 20:20:46 +0800

    Completed lecture 154.

--- 20. Learn Python Intermediate/Numbers.py
+++ 20. Learn Python Intermediate/Numbers.py
@@ -3,6 +3,23 @@
 # a program is instructions that tell a computer what to do
     # all about storing information or a data type and modifying it
 
-# int (interger) - represents all numbers
+# int (interger) - represents all whole numbers 
+print(2 + 4) # addition
+print(2 - 4) # subtraction
+print(2 * 4) # multiplication
+print(2 / 4) # division
 
-# float 
+# can use 'type' to see what data type something is
+print(type(2 + 4)) # 6 (int)
+print(type(2 - 4)) # -2 (int)
+print(type(2 * 4)) # 8 (int)
+print(type(2 / 4)) # 0.5 (float)
+
+# float (floating point number) - represents numbers with decimal point
+    # takes up more memory space than int
+print(type(20.0 + 1.3)) 
+
+# Other operators
+print(2 ** 3) # exponents 
+print(5 // 4) # rounds total down to an int
+print(6 % 4) # modulo - represents remainder of divsion
