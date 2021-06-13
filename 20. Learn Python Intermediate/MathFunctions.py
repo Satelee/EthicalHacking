84f9a83ad8df46c8f33d23ac45ab6ec468cd6107
Satele <soumita.kale@outlook.com>
2021-06-13 20:29:46 +0800

    Completed lecture 155

---
+++ 20. Learn Python Intermediate/MathFunctions.py
@@ -0,0 +1,11 @@
+## Math Functions ##
+
+# rounds to nearest int
+print(round(3.1))
+print(round(3.7))
+
+# returns absolute value of arugment provided (positive value)
+print(abs(-20))
+print(abs(-39.6))
+
+# link to even more math functions to use and explore: https://www.programiz.com/python-programming/modules/math
