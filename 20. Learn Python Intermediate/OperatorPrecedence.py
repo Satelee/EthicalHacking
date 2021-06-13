aa8e49f4f4e2e231f7fa7ee8b53e27c7a15c58aa
Satele <soumita.kale@outlook.com>
2021-06-13 22:05:22 +0800

    completed lecture 157

---
+++ 20. Learn Python Intermediate/OperatorPrecedence.py
@@ -0,0 +1,20 @@
+## Operator Precedence ##
+# Python follows BIMDAS
+
+# B = brackets ()
+# I = indicies **
+# M = multiplication *
+# D = division /
+# A = addition +
+# S = subtraction -
+
+# example
+print((20-3) + 2 ** 2)
+
+# 1. 20-3 is calculated
+# 2. 2^2 is calculated
+# 3. Total of both sides gets added together
+
+# 20 - 3 = 17
+# 2^2 = 4
+# 17 + 4 = 21
