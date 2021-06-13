bd405dfa6f6cb8e0e0ca7f82886012f1c676e045
Satele <soumita.kale@outlook.com>
2021-06-13 22:40:50 +0800

    compelted lecture 159

---
+++ 20. Learn Python Intermediate/Binary&Complex.py
@@ -0,0 +1,15 @@
+## Complex ##
+# another data type (like int & float)
+# only used for doing complex math
+# equivelent to a real number
+
+## Binary Numbers ##
+# returns binary representation of an int
+# '0b' to tell python that number is binary 
+print(bin(7))
+
+# can convert binary number to int
+# need to specify the binary number in brackets + 2 (as binary is base 2) as you want to convert the base 2 number (binary number) to an int
+print(int('0b100000', 2))
+
+# extra resources to understand int function https://www.geeksforgeeks.org/python-int-function/
