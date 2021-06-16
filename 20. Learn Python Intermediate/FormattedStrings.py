f3d1685b945b98c6bdf59482d4fc01afa95ed9d0
Satele <soumita.kale@outlook.com>
2021-06-16 17:06:44 +0800

    Started lecture 167

---
+++ 20. Learn Python Intermediate/FormattedStrings.py
@@ -0,0 +1,7 @@
+## 167. Formatted Strings ##
+
+# adding 'f' to the begining of a print statement and including all variables in {}, it lets python kknow to format the string.
+name = 'Jacob'
+age = 26
+
+print(f'Hi {name}! You are {age} years old.')
