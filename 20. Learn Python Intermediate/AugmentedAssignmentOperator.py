59154adf5a5d31fbc61451e3eabf0462831ccc30
Satele <soumita.kale@outlook.com>
2021-06-16 14:41:57 +0800

    Completed Lecture 162.

---
+++ 20. Learn Python Intermediate/AugmentedAssignmentOperator.py
@@ -0,0 +1,21 @@
+## Augmented Assignment Operator ##
+# add
+some_value = 5
+some_value += 2
+print(some_value)
+# same as saying 'some_value = some_value +2 
+
+# subtract
+some_value2 = 5
+some_value2 -= 2
+print(some_value2)
+# same as saying 'some_value = some_value -2 
+
+# multiply
+some_value3 = 5
+some_value3 *= 2
+print(some_value3)
+# same as saying 'some_value = some_value *2 
+
+# in order for it to work, need to make sure variable has been assigned to a value previously
+# operator comes left to the equals sign
