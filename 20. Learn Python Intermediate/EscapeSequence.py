18ffd2a619c9851e328b21d75e79fca15683357f
Satele <soumita.kale@outlook.com>
2021-06-16 16:35:06 +0800

    Compelted lecture 166.

---
+++ 20. Learn Python Intermediate/EscapeSequence.py
@@ -0,0 +1,16 @@
+## 166. Escape Sequence ##
+weather = "It\'s \"kind of\" sunny."
+print(weather)
+
+weather1 = "It\\s \"kind of\" sunny."
+print(weather1)
+
+# after the '\', python will assume anything after it is a string.
+
+# \t - adds a tab after it.
+string1 = "\t look, im indented."
+print(string1)
+
+# \n - adds a new line.
+string2 = "Today's weather is:\n Rainy!"
+print(string2)
