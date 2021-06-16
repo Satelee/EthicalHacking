74c1b6244eae9bce66bab26462a7a7b00b7ed458
Satele <soumita.kale@outlook.com>
2021-06-16 14:50:41 +0800

    Completed Lecture 163

---
+++ 20. Learn Python Intermediate/Strings.py
@@ -0,0 +1,26 @@
+## Strings ##
+# strings are just a piece of text written by placing them in single or double quotation marks
+# can input numbers and special characters as well
+
+print(type('hi, hello there'))  # prints 'str' = string
+
+# assigning strings to varaibles 
+username = 'supercoder'
+password = "supersecret"
+
+print(username)
+print(password)
+
+# three single quotes used for multi-line strings
+long_string = '''
+WOW
+O O
+---
+'''
+print(long_string)
+
+# add two strings together by using '+' sign 
+first_name = "Satele"
+last_name = "Ephyra"
+full_name = first_name + ' ' + last_name    # quotation in middle acts as a space
+print(full_name)
