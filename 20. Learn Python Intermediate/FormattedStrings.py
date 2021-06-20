42bbe21b0fd1791dfd0dd889a03cf247da7b604a
Satele <soumita.kale@outlook.com>
2021-06-21 02:11:56 +0800

    Finished lecture 167 including exercise.

--- 20. Learn Python Intermediate/FormattedStrings.py
+++ 20. Learn Python Intermediate/FormattedStrings.py
@@ -19,3 +19,27 @@ print("Hi {1}! You are {0} years old".format(name, age))
 
 # you can also create new variables within the formatting but need to include variable name within {}
 print("Hi {name2}! You are {age2} years old".format(name2 = 'Zach', age2 = '30'))
+
+######################################
+
+## Formatted Strings Exercise ##
+
+# 1 What would be the output of the below 4 print statements? 
+#Try to answer these before you click RUN!
+
+print("Hello {}, your balance is {}.".format("Cindy", 50))
+    # Hello Cindy, your balance is 50.
+
+print("Hello {0}, your balance is {1}.".format("Cindy", 50))
+    # Hello Cindy, your balance is 50.
+
+print("Hello {name}, your balance is {amount}.".format(name="Cindy", amount=50))
+    # Hello Cindy, your balance is 50.
+
+print("Hello {0}, your balance is {amount}.".format("Cindy", amount=50))
+    # Hello Cindy, your balance is 50.
+
+# 2 How would you write this using f-string?
+name = 'Cindy'
+amount = 50
+print(f'Hello {name}, your balance is {amount}.')
