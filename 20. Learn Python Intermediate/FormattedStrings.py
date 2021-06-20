fb1d09e3b4b85d2c1a02f3a268bfe35e80597537
Satele <soumita.kale@outlook.com>
2021-06-21 02:41:22 +0800

    Changed formatting of exercise question.

--- 20. Learn Python Intermediate/FormattedStrings.py
+++ 20. Learn Python Intermediate/FormattedStrings.py
@@ -24,8 +24,8 @@ print("Hi {name2}! You are {age2} years old".format(name2 = 'Zach', age2 = '30')
 
 ## Formatted Strings Exercise ##
 
-# 1 What would be the output of the below 4 print statements? 
-#Try to answer these before you click RUN!
+# 1. What would be the output of the below 4 print statements? 
+    # Try to answer these before you click RUN!
 
 print("Hello {}, your balance is {}.".format("Cindy", 50))
     # Hello Cindy, your balance is 50.
@@ -39,7 +39,7 @@ print("Hello {name}, your balance is {amount}.".format(name="Cindy", amount=50))
 print("Hello {0}, your balance is {amount}.".format("Cindy", amount=50))
     # Hello Cindy, your balance is 50.
 
-# 2 How would you write this using f-string?
+# 2. How would you write this using f-string?
 name = 'Cindy'
 amount = 50
 print(f'Hello {name}, your balance is {amount}.')
