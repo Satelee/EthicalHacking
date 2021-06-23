263dc81b77cabed1b5a076cf1b3c0d9d77bffc27
Satele <soumita.kale@outlook.com>
2021-06-23 22:12:10 +0800

    Completed Lecture 171

---
+++ 20. Learn Python Intermediate/Booleans.py
@@ -0,0 +1,18 @@
+## 171. Booleans ##
+# written as 'bool'
+# Only two options: true or false.
+
+name = 'Satele'
+is_cool = False 
+is_cool = True
+
+# booleans represent the idea of having a 'true' & 'false' statement
+# can use booleans to make the computer do something if a statement is true or false.
+
+# using booleans to denote logic
+print(bool(0))  # false
+print(bool(1))  # true
+
+# can convert a string into boolean true or false
+print(type(bool('True')))
+print(type(bool('False')))
