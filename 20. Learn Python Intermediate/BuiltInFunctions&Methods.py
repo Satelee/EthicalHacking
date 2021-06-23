4276f8fed749c3444320f8d3532d4c9ff8bebe22
Satele <soumita.kale@outlook.com>
2021-06-23 19:10:13 +0800

    started lecture 170.

---
+++ 20. Learn Python Intermediate/BuiltInFunctions&Methods.py
@@ -0,0 +1,8 @@
+## 170. Built-In Functions & Methods ##
+
+# calculating length of string
+print(len('helloooooo'))
+# it counts as humans do from 1 instead of starting from 0
+
+greet = 'hihiiiiiii' 
+print(greet[0:])
