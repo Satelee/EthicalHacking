1791ab620bd8d54b48c60896272262d6c064baf2
Satele <soumita.kale@outlook.com>
2021-06-23 22:27:20 +0800

    Completed Lecture 172

---
+++ 20. Learn Python Intermediate/TypeConversionExercise.py
@@ -0,0 +1,12 @@
+## 172. Type Conversion Exercise ##
+
+# program to display user's age based on year
+birth_year = input('what year were you born? ')     # allows user to input year of birth
+
+age = 2021 - int(birth_year)    # calculates age
+
+print('Your age is: ', age)     # prints age to user
+
+
+# for printing, another possible way is to use the 'f' function. e.g.
+# print(f'Your age is: {age}')
