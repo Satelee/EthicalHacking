cf148a13809a8db957b8ed8e1e09434f6470f01b
Satele <soumita.kale@outlook.com>
2021-06-25 14:34:54 +0800

    Completed lecture 175.

---
+++ 20. Learn Python Intermediate/Lists.py
@@ -0,0 +1,19 @@
+## 175. Lists ##
+# ordered sequence of objects that can be any type
+# idenfiy as lists using [] 
+# lists are a form of array - a collection of items
+
+li = [1,2,3,4,5]
+li2 = ['a','b', 'c']
+li3 = [1,2,'a','b', True]
+
+# Data Structure - a way for us to organize information and data 
+    # a container around your daata that has different pros and cons of accessing that data, removing that data and writing data.
+
+# [] allow us to contain information & data like strings, intergers floats, booleans
+
+amazon_cart = ['notebooks', 'sunglasses']
+
+# can print which item from the list you want to see
+print(amazon_cart[0])
+print(amazon_cart[1])
