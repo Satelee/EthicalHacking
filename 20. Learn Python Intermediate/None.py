fa7edb16679a19febea3d06ddf924b940e669ed5
Satele <soumita.kale@outlook.com>
2021-06-26 15:14:53 +0800

    Completed lecture 183

---
+++ 20. Learn Python Intermediate/None.py
@@ -0,0 +1,8 @@
+## 183. None ##
+# None is a special data type that exists in python
+# represents the absense of value
+
+# a user in a game has weapons, but they do not have weapons when they start
+# None can be used to represent the empty starting weapons inventory
+weapons = None
+print(weapons) 
