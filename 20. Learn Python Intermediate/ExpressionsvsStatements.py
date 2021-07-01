55625247a19a750575d811bb551e86a7a8fef3f6
Satele <soumita.kale@outlook.com>
2021-07-01 17:12:35 +0800

    changed file name to match other formatting.

---
+++ 20. Learn Python Intermediate/ExpressionsvsStatements.py
@@ -0,0 +1,12 @@
+## 161. Expression vs Statements ##
+iq = 100
+user_age = iq/5
+
+print(user_age)
+
+# expression is a piece of code that produces a value.
+    # in this case it would be 'iq/5'
+
+# statement is an entire line of code that performs an action
+    # in this case it would be 'user_age = iq/5' as the action is calculating 100/5 and assigning the value to 'user_age' variable
+    # 'iq = 100' would also be a statement
